d95fc10dff0105c7f4e273ca8ad0691c4b17036c
Nitin K <nitink61996@gmail.com>
2022-07-27 12:30:29 +0530

    Add mock functionality for server - Jest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,15 +1,58 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('TEST GET /LAUNCH', () => {
-    const response = 200;
-    test('It should give success 200', () => {
-        expect(response).toBe(200);
+    test('It should give success 200', async () => {
+        const response = await request(app)
+            .get('/launches')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        // expect(response.statusCode).toBe(200);
     });
 });
 
 describe('Test Post /launch', () => {
-    const response = 400;
-    test('It should give success 200', () => {});
+    const completeLaunchData = {
+        mission: 'USS Enterprises',
+        rocket: 'NCC ufd -1',
+        target: 'Keplet 186 f',
+        launchDate: 'January 4,2028',
+    };
 
-    test('It should throw error for validations', () => {
-        expect(response).toBe(400);
+    const launchDataWithoutDate = {
+        mission: 'USS Enterprises',
+        rocket: 'NCC ufd -1',
+        target: 'Keplet 186 f',
+    };
+
+    const invalidLaunchData = {
+        mission: 'USS Enterprises',
+        rocket: 'NCC ufd -1',
+        target: 'Keplet 186 f',
+    };
+
+    test('It should give success 200', async () => {
+        const response = await request(app)
+            .post('/addNewLaunch')
+            .send(completeLaunchData)
+            .expect(201);
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+
+        expect(requestDate).toBe(responseDate);
+
+        expect(response.body).toMatchObject(launchDataWithoutDate);
+    });
+
+    test('It should throw error for validations for Missing Required Fields', async () => {
+        const response = await request(app)
+            .post('/addNewLaunch')
+            .send(invalidLaunchData)
+            .expect(400);
+
+        expect(response.body).toStrictEqual({
+            error: 'Missing Required Fields',
+        });
     });
 });
