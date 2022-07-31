9d3a04a475c818eee8f17f990fdbdf92bb9df107
Nitin K <nitink61996@gmail.com>
2022-07-31 21:10:29 +0530

    Added MongoDB to project

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const app = require('./app');
 
+const mongoose = require('mongoose');
+
 const { loadPlanetsData } = require('./models/planets.model');
 
 if (process.env.NODE_ENV !== 'production') {
@@ -10,13 +12,24 @@ if (process.env.NODE_ENV !== 'production') {
 
 const PORT = process.env.PORT || 8000;
 
+const DATABASE_URL = process.env.MONGO_DB_URL;
+
 const server = http.createServer(app);
 
 async function startServer() {
-    await loadPlanetsData();
-    server.listen(PORT, () => {
-        console.log(`Server is started on PORT : ${PORT}`);
-    });
+    await mongoose
+        .connect(DATABASE_URL)
+        .then(async () => {
+            console.log('Server has been connected to the Database');
+
+            await loadPlanetsData();
+            server.listen(PORT, () => {
+                console.log(`Server is started on PORT : ${PORT}`);
+            });
+        })
+        .catch((err) => {
+            console.log('Error connecting to the database', err);
+        });
 }
 
 startServer();
